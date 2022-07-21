946408a238851b1da17982f9efa3fcd73f9f7194
Lukas Martini <hello@lutoma.org>
2022-07-21 04:42:04 +0200

    Properly resolve config file from env var

--- src/config.py
+++ src/config.py
@@ -4,7 +4,13 @@ from typing import List
 from confz import ConfZ, ConfZFileSource
 from pydantic import AnyUrl
 
-CONFIG_DIR = Path(__file__).resolve().parent.parent
+
+def get_config_file():
+    env_path = environ.get('SHORTDIARY_CONFIG')
+    if env_path:
+        return Path(env_path)
+
+    return Path(__file__).resolve().parent.parent / 'config.yml'
 
 
 class StripeConfig(ConfZ):
@@ -19,5 +25,4 @@ class APIConfig(ConfZ):
     jwt_secret: str
     stripe: StripeConfig
 
-    CONFIG_SOURCES = ConfZFileSource(file=environ.get(
-        'SHORTDIARY_CONFIG', CONFIG_DIR / 'config.yml'))
+    CONFIG_SOURCES = ConfZFileSource(file=get_config_file())
